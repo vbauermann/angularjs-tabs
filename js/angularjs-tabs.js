d91772205dc5280aa75b611ed7bb34d6a57ddfcd
Vitor Bauermann Silveira <40840614+vbauermann@users.noreply.github.com>
2019-05-20 15:35:53 -0300

    Update angularjs-tabs.js

--- js/angularjs-tabs.js
+++ js/angularjs-tabs.js
@@ -6,34 +6,37 @@
  * @licence: http://opensource.org/licenses/MIT
  */
 
-;(function(angular){
+; (function (angular) {
   var app = angular.module('angularjsTabs', []);
-  app.directive('tabNav', function(){ 
-    return{
+  app.directive('tabNav', function () {
+    return {
       restrict: 'A',
-      link: function(scope, element, attrs, ctrl){
-        var tabTriggers = element[0].children; 
+      link: function (scope, element, attrs, ctrl) {
+        var tabTriggers = element[0].children;
         var tabGroup = attrs.tabGroup;
-        var showTab = function(triggerEle){
-          for(var j=0; j<tabTriggers.length; j++){
+        var activeClass = attrs.activeClass;
+        if (!activeClass)
+          activeClass = 'active';
+        var showTab = function (triggerEle) {
+          for (var j = 0; j < tabTriggers.length; j++) {
             var trigger = tabTriggers[j].children[0];
-            trigger.classList.remove('active');
+            trigger.classList.remove(activeClass);
           }
-          var tabsInGroup = document.querySelectorAll('.tab-panel[data-tab-group="'+tabGroup+'"]');
-          for(var j=0; j<tabsInGroup.length; j++){
+          var tabsInGroup = document.querySelectorAll('.tab-panel[data-tab-group="' + tabGroup + '"]');
+          for (var j = 0; j < tabsInGroup.length; j++) {
             tabsInGroup[j].style.display = "none";
           }
           var targetTabName = triggerEle.getAttribute('data-target-tab');
-          var targetTab = document.querySelectorAll('.tab-panel[data-tab-group="'+tabGroup+'"][data-tab-name="'+targetTabName+'"]')[0];
-          triggerEle.classList.add('active');
+          var targetTab = document.querySelectorAll('.tab-panel[data-tab-group="' + tabGroup + '"][data-tab-name="' + targetTabName + '"]')[0];
+          triggerEle.classList.add(activeClass);
           targetTab.style.display = "block";
         }
-        for(var i=0; i<tabTriggers.length; i++){
+        for (var i = 0; i < tabTriggers.length; i++) {
           var trigger = tabTriggers[i].children[0];
-          if(trigger.classList.contains('active')){
+          if (trigger.classList.contains(activeClass)) {
             showTab(trigger);
           }
-          trigger.addEventListener('click', function(e){
+          trigger.addEventListener('click', function (e) {
             e.preventDefault();
             showTab(this);
           });
